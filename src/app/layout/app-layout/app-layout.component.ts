7119fc3332c16619fe99de04f2dfb9effd6e2c22
nadherarroum <nadher.arroum@gmail.com>
2025-07-02 00:03:57 +0100

    update layout

--- src/app/layout/app-layout/app-layout.component.ts
+++ src/app/layout/app-layout/app-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SidebarItem } from '../../shared/models/sidebar-item';
+import { EmployeeAvatar } from '../../models/employees/employee-avatar';
 
 @Component({
   selector: 'app-layout',
@@ -8,9 +10,35 @@ import { Component } from '@angular/core';
 export class AppLayoutComponent {
 
   isSidebarCollapsed = false;
+  currentPage: SidebarItem | null = null;
+
+  allAvatars: EmployeeAvatar[] = [
+    { name: 'Nadher', photoUrl: 'assets/images/default-avatar.png' },
+    { name: 'Bob', photoUrl: 'assets/images/img-3.jpg' },
+    { name: 'Charlie', photoUrl: 'assets/images/img-2.jpg' },
+    { name: 'Diana', photoUrl: 'assets/images/img-4.png' },
+    { name: 'Eve', photoUrl: 'assets/images/img-1.jpg' },
+  ];
+
+  maxDisplayedAvatars = 4;
+
+  get displayedAvatars(): EmployeeAvatar[] {
+    return this.allAvatars.slice(0, this.maxDisplayedAvatars);
+  }
+
+  get extraAvatarsCount(): number {
+    return Math.max(0, this.allAvatars.length - this.maxDisplayedAvatars);
+  }
 
   toggleSidebar() {
     this.isSidebarCollapsed = !this.isSidebarCollapsed;
   }
 
+  onCurrentPageChange(page: SidebarItem) {
+    this.currentPage = page;
+  }
+
+  onAddEmployee() {
+    console.log('Ajouter un employee!');
+  }
 }
